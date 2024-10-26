bb7ef2f5896754c1fe77fb03b5d76622bbd19aaa
yuhldr <yuhldr@qq.com>
2024-10-26 13:28:37 +0800

    fix: 不保留session

--- heartale/servers/legado.py
+++ heartale/servers/legado.py
@@ -12,6 +12,7 @@ from heartale.tools import data2url, split_text
 CHAP_POS = "durChapterPos"
 CHAP_INDEX = "durChapterIndex"
 CHAP_TITLE = "durChapterTitle"
+CHAP_TXT_N = "durChapterTxtN"
 
 
 def get_base_url(conf):
@@ -24,6 +25,7 @@ def get_base_url(conf):
         str: _description_
     """
     return f'http://{conf["ip"]}:{conf["port"]}'
+
 
 def bu(book_data: dict):
     """_summary_
@@ -47,28 +49,31 @@ class LegadoServer(Server):
             conf (dict): 配置 conf["legado"]
         """
         # 书籍信息
-        self.book_data = None
+        self.book_data = {
+            # 当前读到txts的第几个了
+            CHAP_TXT_N: 0,
+            # 章节序号，按照self.cls计算的
+            CHAP_INDEX: 0,
+            # 位置
+            CHAP_POS: 0,
+        }
         # 章节名字
         self.cls = None
-
-        # 章节序号，按照self.cls计算的
-        self.chap_n = 0
-
+        # 现在的章节是第几个，前面的目录不要
+        self.chap_n0 = 0
         # 要阅读的，并且分割好的文本list
         self.txts = []
-        # 当前读到txts的第几个了
-        self.txt_n = 0
         # 每个 txt_n 对应的在原文中的位置
         self.p2s = []
         super().__init__("legado")
 
     async def initialize(self):
-        """异步初始化"""
 
         self.book_data = await self.get_book_shelf(0)
-
+        self.book_data[CHAP_TXT_N] = 0
         self.cls = await self.get_chapter_list(self.book_data)
-        self.cls = self.cls[self.book_data[CHAP_INDEX]:]
+        self.chap_n0 = self.book_data[CHAP_INDEX]
+        self.cls = self.cls[self.chap_n0:]
         self.book_name = self.book_data["name"]
 
         # 只取之后的章节名字，最多100章
@@ -84,25 +89,24 @@ class LegadoServer(Server):
             _type_: _description_
         """
 
-        if self.txt_n == len(self.txts):
+        if self.book_data[CHAP_TXT_N] == len(self.txts):
             # 第一个是标题
             if len(self.txts) > 1:
-                self.txt_n = 0
-                self.chap_n += 1
+                self.book_data[CHAP_TXT_N] = 0
                 self.book_data[CHAP_POS] = 0
                 self.book_data[CHAP_INDEX] += 1
-                self.book_data[CHAP_TITLE] = self.cls[self.chap_n]
+                self.book_data[CHAP_TITLE] = self.cls[self.book_data[CHAP_INDEX]-self.chap_n0]
 
             book_txt = await self.get_book_txt(self.book_data)
-            self.txts, self.p2s, self.txt_n = split_text(
+            self.txts, self.p2s, self.book_data[CHAP_TXT_N] = split_text(
                 book_txt, self.book_data[CHAP_POS])
 
-            return self.cls[self.chap_n]
+            return self.cls[self.book_data[CHAP_INDEX]-self.chap_n0]
 
-        txt = self.txts[self.txt_n]
-        self.book_data[CHAP_POS] = self.p2s[self.txt_n]
+        txt = self.txts[self.book_data[CHAP_TXT_N]]
+        self.book_data[CHAP_POS] = self.p2s[self.book_data[CHAP_TXT_N]]
         await self.save_book_progress(self.book_data)
-        self.txt_n += 1
+        self.book_data[CHAP_TXT_N] += 1
         return txt
 
     async def get_book_shelf(self, book_n: int):
